2743af51df9c83c016cccc47fea130ab76136bb3
yadesh.desai <yadesh@gmail.com>
2026-06-10 08:48:36 +0530

    version-1

---
+++ p1.py
@@ -0,0 +1,22 @@
+a="madam"
+
+if a==a[::-1]:
+   print("palandrom")
+else:
+   print("not palandrom")
+
+
+a1=int(input("enter num"))
+
+if a1%2==0:
+   print("even")
+else:
+   print("odd")
+
+
+z1=input("enter a word")
+
+if z1==z1[::-1]:
+   print("palandrom")
+else:
+   print("not palandrom")
