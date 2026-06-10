aa2950cc02f1e8fe45cfdefdc86a4dd10d64898f
yadesh.desai <yadesh@gmail.com>
2026-06-10 09:11:46 +0530

    version-3 - withdraw feature added

--- atm.py
+++ atm.py
@@ -1,7 +1,10 @@
 balance = 1000
 
 deposit = int(input("Enter deposit amount: "))
-balance = balance + deposit
+balance += deposit
+
+withdraw = int(input("Enter withdraw amount: "))
+balance -= withdraw
 
 print("Current Balance:", balance)
 
